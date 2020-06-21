c955957199206b87e3b81b05ce61e0970681b097
Davy Souza <davy.souza@outlook.com>
2020-06-21 00:08:20 -0300

    Issue #2: Importing GTests for unit tests

---
+++ require.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+# install dependencies
+sudo apt install gcc g++ libgtest-dev -y
+
+# install cmake
+sudo apt-get install cmake -y
+
+# compile gtest
+cd /usr/src/gtest
+sudo cmake CMakeLists.txt
+sudo make
+ 
+# copy or symlink libgtest.a and libgtest_main.a to your /usr/lib folder
+sudo cp *.a /usr/lib
